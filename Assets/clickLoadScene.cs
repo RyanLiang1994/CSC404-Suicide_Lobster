882f2e91f4c908c52720339d943ccff0651713e7
Ryan <chengjie.liang@mail.utoronto.ca>
2018-09-22 11:00:10 -0400

    Add a starting scene and a couple of buttons

---
+++ Assets/clickLoadScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class clickLoadScene : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
